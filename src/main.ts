b887034d4d6f608c61627e0bea66c54e9ce91871
Joao Moreno <mail@joaomoreno.com>
2019-11-02 17:28:44 +0100

    sproc

--- src/main.ts
+++ src/main.ts
@@ -6,25 +6,25 @@ interface Counter {
   value: number;
 }
 
-async function work(id): Promise<void> {
-  for (let i = 0; i < 10; i++) {
-    console.log(i);
-    const client = new CosmosClient(opts);
-    const item = await client.database('tests').container('counters').item(id, id);
-    const res = await item.read<Counter>();
-    const counter = res.resource;
-    counter.value++;
-    await item.replace(counter);
-    await new Promise(c => setTimeout(c, 500 + Math.random() * 100));
-  }
-}
-
-async function main(): Promise<void> {
+async function testMultipleClients(): Promise<void> {
   const id = '' + new Date();
 
   const client = new CosmosClient(opts);
   await client.database('tests').container('counters').items.create({ id, value: 0 });
   console.log('Created document', id);
+
+  async function work(id: string): Promise<void> {
+    for (let i = 0; i < 10; i++) {
+      console.log(i);
+      const client = new CosmosClient(opts);
+      const item = await client.database('tests').container('counters').item(id, id);
+      const res = await item.read<Counter>();
+      const counter = res.resource;
+      counter.value++;
+      await item.replace(counter);
+      await new Promise(c => setTimeout(c, 100 + Math.random() * 100));
+    }
+  }
 
   const promises = [
     work(id),
@@ -41,6 +41,47 @@ async function main(): Promise<void> {
   const res = await item.read<Counter>();
   const counter = res.resource;
   console.log('counter:', counter.value);
+}
+
+async function testSProc(): Promise<void> {
+  const id = '' + new Date();
+
+  const client = new CosmosClient(opts);
+  await client.database('tests').container('counters').items.create({ id, value: 0 });
+  console.log('Created document', id);
+
+  async function work(id: string): Promise<void> {
+    for (let i = 0; i < 10; i++) {
+      console.log(i);
+      const client = new CosmosClient(opts);
+
+      const sproc = await client.database('tests').container('counters').scripts.storedProcedure('increment');
+      await sproc.execute(id, [id]);
+      console.log(i);
+      await new Promise(c => setTimeout(c, 100 + Math.random() * 100));
+    }
+  }
+
+  const promises = [
+    work(id),
+    work(id),
+    work(id),
+    work(id),
+    work(id)
+  ];
+
+  await Promise.all(promises);
+  console.log('Done');
+
+  const item = await client.database('tests').container('counters').item(id, id);
+  const res = await item.read<Counter>();
+  const counter = res.resource;
+  console.log('counter:', counter.value);
+}
+
+async function main(): Promise<void> {
+  // await testMultipleClients();
+  await testSProc();
 }
 
 main().then(
